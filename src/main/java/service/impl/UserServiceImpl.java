768a4fb8a6634fb04f2e1086fd72f00be5b5fc53
SilentLikeSummer <531054904@qq.com>
2017-07-19 09:39:19 +0800

    Update UserServiceImpl.java

--- src/main/java/service/impl/UserServiceImpl.java
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -1,19 +1,24 @@
 package service.impl;
 
 import java.io.File;
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
-import dao.BookReleaseDao;
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
 import dao.UserDao;
-import model.BookRelease;
 import model.User;
-import model.Picture;
 import service.UserService;
+import utils.MD5Util;
+import utils.EmailUtil;
 
 public class UserServiceImpl implements UserService {
 
 	private UserDao userDao;
-	private BookReleaseDao bookReleaseDao;
+	private String activateUrl;
 
 	public UserDao getUserDao() {
 		return userDao;
@@ -22,13 +27,12 @@ public class UserServiceImpl implements UserService {
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
-
-	public BookReleaseDao getBookReleaseDao() {
-		return bookReleaseDao;
+	public String getActivateUrl() {
+		return activateUrl;
 	}
 
-	public void setBookReleaseDao(BookReleaseDao bookReleaseDao) {
-		this.bookReleaseDao = bookReleaseDao;
+	public void setActivateUrl(String activateUrl) {
+		this.activateUrl = activateUrl;
 	}
 
 	public Integer save(User user) {
@@ -47,10 +51,6 @@ public class UserServiceImpl implements UserService {
 		return userDao.getUserById(id);
 	}
 
-    public Picture getPictureById(int id){
-    	return userDao.getPictureById(id);
-    }
-	
 	public User getUserByEmail(String email) {
 		return userDao.getUserByEmail(email);
 	}
@@ -91,15 +91,54 @@ public class UserServiceImpl implements UserService {
 		return userDao.uploadPicture(id, image, contentType, fileName);
 	}
 
-	public boolean register(User user) {
-		if(getUserByEmail(user.getEmail()) != null){
+	public boolean register(User user) throws AddressException, MessagingException {
+		if (getUserByEmail(user.getEmail()) != null) {
 			return false;
 		}
+
+		/* 将用户邮箱和密码进行加密并发送验证邮件 */
+		String dest = user.getEmail();
+		String token = dest + user.getPassword();
+		String md5Token = MD5Util.MD5(token);
+		user.setToken(md5Token);
+		String title = "欢迎来到阅享";
+		String content = "<p>您好，" + user.getNickname() + "！</p><br/><br/>账户需要激活才能使用，请在24小时内点击下面的链接激活：<br/><a href='"
+				+ activateUrl + "?token=" + md5Token + "&email=" + dest + "'>"
+				+ activateUrl + "?token=" + md5Token + "&email=" + dest + "</a>";
+		EmailUtil.sendMail(dest, title, content);
+
+		/* 设置激活期限，限制一天内完成激活 */
+		java.util.Date date = new java.util.Date(System.currentTimeMillis());
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(date);
+		calendar.add(Calendar.DATE, 1);// 把日期往后增加一天.整数往后推,负数往前移动
+		Date activateDue = new Date(calendar.getTime().getTime()); // 这个时间就是日期往后推一天的结果
+		user.setActivateDue(activateDue);
+		
+		/* 将用户信息存入数据库 */
 		int userId = userDao.save(user);
 		user.setId(userId);
 		return true;
 	}
 
+	public boolean activateUser(String token, String email) {
+		User u = userDao.getUserByEmail(email);
+		if (u == null) {
+			return false;
+		}
+		if (!u.getToken().equals(token)) {
+			return false;
+		}
+
+		/* 只将状态为"未激活"的用户状态设置为"已激活"，防止将管理员或被封禁用户设置为"已激活" */
+		if (u.getRole() == 0) {
+			u.setRole(1);
+			/* 重新设置token防止重复激活 */
+			u.setToken("ACTIVATED");
+			userDao.update(u);
+		}
+		return true;
+	}
 
 	public void manageUser(int id, int status) {
 		User u = getUserById(id);
@@ -114,19 +153,4 @@ public class UserServiceImpl implements UserService {
 		}
 	}
 
-
-
-
-
-
-
-	public void checkBookRelease(BookRelease bookRelease, short status) {
-		if (status == 1) {
-			bookRelease.setStatus((short) 1);
-			bookReleaseDao.update(bookRelease);
-		} else if (status == 0) {
-			bookRelease.setStatus((short) -1);
-			bookReleaseDao.update(bookRelease);
-		}
-	}
 }
