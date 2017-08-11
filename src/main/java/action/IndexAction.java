3882e07d242f38bbacb8c64efbf68fad549388a4
ciguicigui <cgcg96@sjtu.edu.cn>
2017-08-11 08:48:32 +0800

    fix bugs

--- src/main/java/action/IndexAction.java
+++ src/main/java/action/IndexAction.java
@@ -53,7 +53,7 @@ public class IndexAction extends BaseAction {
 		request().setAttribute("categories", categories);
 		
 		if(session().getAttribute("userId") != null){
-			User u = userService.getUserById((int)session().getAttribute("userId"));
+			User u = userService.getUserById((Integer)session().getAttribute("userId"));
 			List<Book> recBooks = bookService.getBooksByCategory(u.getFav_category());
 			Collections.shuffle(recBooks);
 			request().setAttribute("recommendBooks", recBooks);
