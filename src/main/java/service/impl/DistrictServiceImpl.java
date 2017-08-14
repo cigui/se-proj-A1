d3740ec499bad93edac9a422ea3deac1b5bbc9e0
cigui-PC\cigui <cgcg96@sjtu.edu.cn>
2017-08-14 16:19:46 +0800

    修改getDistrictById方法的实现

--- src/main/java/service/impl/DistrictServiceImpl.java
+++ src/main/java/service/impl/DistrictServiceImpl.java
@@ -1,6 +1,8 @@
 package service.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import dao.DistrictDao;
 import model.District;
@@ -9,6 +11,7 @@ import service.DistrictService;
 public class DistrictServiceImpl implements DistrictService {
 
 	private DistrictDao districtDao;
+	private Map<Integer, District> districtsMap;
 	
 	public DistrictDao getDistrictDao() {
 		return districtDao;
@@ -18,12 +21,25 @@ public class DistrictServiceImpl implements DistrictService {
 		this.districtDao = districtDao;
 	}
 
+	/* 
+	 * Modified on 14th Aug, 2017 by Zhou Xin
+	 * Use HashMap to store districts in memory in order to enhance
+	 * performance.
+	 */
 	public District getDistrictById(int id) {
-		return districtDao.getDistrictById(id);
+		if (districtsMap == null) {
+			districtsMap = new HashMap<Integer, District>();
+			List<District> districts = districtDao.getAllDistricts();
+			for (District district : districts) {
+				districtsMap.put(district.getId(), district);
+			}
+		}
+		return districtsMap.get(id);
+		// return districtDao.getDistrictById(id);
 	}
 
 	public List<District> getDistrictsByParent_id(int parent_id) {
-		return districtDao.getDistrictByParent_id(parent_id);
+		return districtDao.getDistrictsByParent_id(parent_id);
 	}
 }
 
