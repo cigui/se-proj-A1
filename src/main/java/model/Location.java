bb316d4e5da5d641908c01e3fb4cadbb21f66cc8
cigui-PC\cigui <cgcg96@sjtu.edu.cn>
2017-08-24 09:38:06 +0800

    用于包装省市区信息的类

---
+++ src/main/java/model/Location.java
@@ -0,0 +1,35 @@
+package model;
+
+/**
+ * Location: 用于包装省、市、区信息的类
+ */
+public class Location {
+	private String province;
+	private String city;
+	private String district;
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+}
